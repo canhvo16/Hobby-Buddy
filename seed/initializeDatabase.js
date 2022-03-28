ca2ad38168a76a1ae7566fe1465914b186e23ff6
canhvo16 <canhvo16@gmail.com>
2022-03-28 10:44:33 -0500

    initial test database created

---
+++ seed/initializeDatabase.js
@@ -0,0 +1,43 @@
+const db = require('../db')
+const { Person, Post, Group } = require('../models')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+  const person1 = await new Person({
+    name: 'Canh',
+    photo:
+      'https://img.freepik.com/free-vector/happy-sushi-salmon-flat-cartoon-style_138676-2635.jpg?size=338&ext=jpg',
+    age: 24,
+    location: 'Chicago, IL',
+    hobbies: 'Volleyball, Cooking, Lifting',
+    interests: 'Coding, Making Tiktoks'
+  })
+  await person1.save()
+
+  const post1 = await new Post({
+    name: [person1._id],
+    text: 'This is my first post!',
+    likes: [person1._id]
+  })
+  await post1.save()
+
+  const group1 = await new Group({
+    owner: [person1._id],
+    name: 'My First Group',
+    members: [person1._id]
+  })
+  await group1.save()
+
+  console.log("Canh's profile, first post, and first group created!")
+}
+
+const run = async () => {
+  await Person.deleteMany()
+  await Post.deleteMany()
+  await Group.deleteMany()
+  await main()
+  db.close()
+}
+
+run()
