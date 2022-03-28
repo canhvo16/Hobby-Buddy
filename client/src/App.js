1404e19d63a2fe23264b20957ad3812f3868b8b1
canhvo16 <canhvo16@gmail.com>
2022-03-28 15:18:54 -0500

    created login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Nav from './components/Nav'
+import LoginPage from './pages/LoginPage'
 import './style/App.css'
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  let nav = isLoggedIn ? <Nav /> : null
+  let nav = isLoggedIn ? <Nav /> : <Link to="/about">About</Link>
 
   return (
     <div className="App">
       {nav}
       <main>
-        <Routes></Routes>
+        <Routes>
+          <Route index element={<LoginPage />} />
+        </Routes>
       </main>
     </div>
   )
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,16 @@
 const LoginPage = () => {
-  return <div></div>
+  return (
+    <div>
+      <h1 className="title">Hobby Buddy</h1>
+      <form className="login">
+        <h3>Username</h3>
+        <input type="text" placeholder="Enter Username" required />
+        <h3>Password</h3>
+        <input type="text" placeholder="Enter Password" required />
+        <button>Login</button>
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
