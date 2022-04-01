812f1ecfc4747f19ec98acf98e22aa7036c1871b
canhvo16 <canhvo16@gmail.com>
2022-04-01 09:46:48 -0500

    deployment

--- db/index.js
+++ db/index.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
+
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb+srv://canhvo16:hobbybuddy@hobby-buddy.lrclf.mongodb.net/hobbyBuddyDatabase?retryWrites=true&w=majority'
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/hobbyBuddyDatabase')
+  .connect(dbUrl)
   .then(() => {
     console.log('Successfully connected to mongoDB!')
   })
--- server.js
+++ server.js
@@ -11,9 +11,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
+app.use(express.static(`${__dirname}/client/build`))
 
 app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
